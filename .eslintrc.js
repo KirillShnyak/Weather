225ca8c31c42669ea088160b1d593d5497a133fe
KirillShnyak <kshnyak@bk.ru>
2023-06-17 21:48:37 +0500

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+        code: 200,
+      },
+    ],
+    'no-plusplus': 'off',
+    'no-alert': 'off',
+    'consistent-return': 'off',
+    eqeqeq: 'off',
+    'no-param-reassign': 'off',
+
+  },
+};
