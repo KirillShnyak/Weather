9c67dc9a6297937fe6e4c2b66942ad8b0704f7da
KirillShnyak <kshnyak@bk.ru>
2023-06-17 22:18:18 +0500

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,28 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true,
+    "jest/globals": true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['jest'],
+  plugins: ["jest"],
   rules: {
-    'import/prefer-default-export': 'off',
-    'max-len': [
-      'error',
+    "import/prefer-default-export": "off",
+    "max-len": [
+      "error",
       {
         ignoreComments: true,
         code: 200,
       },
     ],
-    'no-plusplus': 'off',
-    'no-alert': 'off',
-    'consistent-return': 'off',
-    eqeqeq: 'off',
-    'no-param-reassign': 'off',
-
+    "no-plusplus": "off",
+    "no-alert": "off",
+    "consistent-return": "off",
+    eqeqeq: "off",
+    "no-param-reassign": "off",
   },
 };
